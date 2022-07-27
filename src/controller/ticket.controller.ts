7a53f2fd3b09fd95ef380efc104e6dbf96727477
toundji <olatoundjiagbahoungba@gmail.com>
2022-07-27 13:06:10 +0100

    .....

--- src/controller/ticket.controller.ts
+++ src/controller/ticket.controller.ts
@@ -6,18 +6,28 @@ import { Ticket } from "src/entities/ticket.entity";
 import { TicketStatus } from "src/enums/ticket-status";
 import { TicketService } from './../services/ticket.service';
 
-
+import {
+  WebSocketServer,
+  
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @ApiTags('Tikects')
 @Controller('tickets')
 export class TikectController {
+  @WebSocketServer()
+  server: Server;
   constructor(
     private readonly ticketService: TicketService,
   ) {}
 
   @Get("create")
-   createOne(): Promise<Ticket> {
-    return  this.ticketService.create();
+   async createOne(): Promise<Ticket> {
+    const ticket: Ticket = await this.ticketService.create();
+    this.server.emit(`onCreate`, ticket);
+    this.emitAll();
+    this.server.emit(`last-order`, +ticket.order_nber );
+    return ticket;
   }
 
   @Get()
@@ -38,6 +48,27 @@ export class TikectController {
   @Get("status/:status")
   geticketByStatus(@Param('status') status:TicketStatus): Promise<Ticket[]> {
    return this.ticketService.findByStatus(status);
+  }
+
+
+  async emitAll(){
+    await this.emitWaiter();
+    await this.emitReceive();
+    await this.emitCancelOfDay();
+  }
+
+  
+  async emitWaiter(){
+    const waiters: Ticket[] = await this.ticketService.findWaiter();
+    this.server.emit(`waiter-list`, waiters);
+  }
+  async emitReceive(){
+    const receivers: Ticket[] = await this.ticketService.findByStatus(TicketStatus.RECEIVE);
+    this.server.emit(`receive-list`, receivers);
+  }
+  async emitCancelOfDay(){
+    const cancels: Ticket[] = await this.ticketService.findCancelOfDay();
+    this.server.emit(`canceled-today`, cancels);
   }
 
 }
